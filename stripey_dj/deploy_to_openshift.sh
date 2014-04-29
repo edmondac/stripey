54d24959a711a15f61daac45f8f8eefbe49a8d8d
roo <devnull@localhost>
2014-04-29 16:57:23 +0100

    Deploying to openshift
    Former-commit-id: 5c9f6c9ad4186f4532cb6588b09c8cae9aabf417

--- stripey_dj/deploy_to_openshift.sh
+++ stripey_dj/deploy_to_openshift.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+function fatal () {
+    echo ${1}
+    exit 1
+}
+
 echo "Commit locally and deploy to openshift? [Y/n]"
 read ok
-if [[ ${ok} == 'n' ]]; then
-    echo "Aborting"
-    exit 1
-fi
+[[ ${ok} == 'n' ]] && fatal "Aborting"
+
+set -x
+set -e
 
 hg commit -m "Deploying to openshift"
 
-ls
+TEMPDIR="/tmp/deploy_to_openshift_tempdir"
+
+[[ -e ${TEMPDIR} ]] && fatal "${TEMPDIR} exists - aborting"
+
+mkdir ${TEMPDIR}
+
+
+
+rm -rf ${TEMPDIR}
