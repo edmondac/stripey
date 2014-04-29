e30665711fa4391f836d5f8e4123935743ea306c
roo <devnull@localhost>
2014-04-29 17:15:35 +0100

    Deploying to openshift
    Former-commit-id: 9e6fe874b782b1d70f05cc3b26192d2b293f82f5

--- stripey_dj/deploy_to_openshift.sh
+++ stripey_dj/deploy_to_openshift.sh
@@ -15,7 +15,7 @@ set -e
 hg commit -m "Deploying to openshift"
 
 OPENSHIFT_REPO="/home/ed/openshift/django/wsgi/openshift/"
-(cd ${OPENSHIFT_REPO} && git rm stripey_app)
+#(cd ${OPENSHIFT_REPO} && git rm stripey_app)
 cp -r stripey_app ${OPENSHIFT_REPO}
 cd ${OPENSHIFT_REPO}
 find stripey_app -name "*.pyc" -exec rm {} ';'
