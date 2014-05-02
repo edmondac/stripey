10c5eb7cf4d7315d0a636224435eafea1d209af1
roo <devnull@localhost>
2014-05-02 16:01:38 +0100

    Deploying to openshift
    Former-commit-id: 61dbee08e189cd4b7b7e210a35ebed04fd37d403

--- stripey_dj/stripey_app/views.py
+++ stripey_dj/stripey_app/views.py
@@ -23,10 +23,10 @@ def index(request):
     """
     The home page. Show a list of all manuscript transcriptions loaded.
     """
-    misc_mss = ManuscriptTranscription.objects.all().filter(liste_id_lt=10000).order_by('liste_id')
-    pap_mss = ManuscriptTranscription.objects.all().filter(liste_id_lt=20000).filter(liste_gte=10000).order_by('liste_id')
-    maj_mss = ManuscriptTranscription.objects.all().filter(liste_id_lt=30000).filter(liste_gte=20000).order_by('liste_id')
-    min_mss = ManuscriptTranscription.objects.all().filter(liste_id_gte=30000).order_by('liste_id')
+    misc_mss = ManuscriptTranscription.objects.all().filter(liste_id__lt=10000).order_by('liste_id')
+    pap_mss = ManuscriptTranscription.objects.all().filter(liste_id__lt=20000).filter(liste__gte=10000).order_by('liste_id')
+    maj_mss = ManuscriptTranscription.objects.all().filter(liste_id__lt=30000).filter(liste__gte=20000).order_by('liste_id')
+    min_mss = ManuscriptTranscription.objects.all().filter(liste_id__gte=30000).order_by('liste_id')
 
     books = Book.objects.all().order_by('num')
     # We want a list of chapters - per book.
