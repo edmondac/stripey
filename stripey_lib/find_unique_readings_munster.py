e48c84fa265fac46c86143bd14b28294b97c8745
roo <devnull@localhost>
2015-03-31 13:04:58 +0100

    auto-commit from ed@roo
    Former-commit-id: 8d1acb16b791a3631f28c64dd82fa2dccbad0ad2

--- stripey_lib/find_unique_readings_munster.py
+++ stripey_lib/find_unique_readings_munster.py
@@ -5,13 +5,18 @@ Find readings unique to two (or more) manuscripts in a Muenster mysql database
 
 import MySQLdb
 import sys
+import itertools
 
 
-def compare(host, db, user, password, table, witnesses):
+def compare(host, db, user, password, table, witnesses, quiet=False):
     """
     Connect to the mysql db and loop through what we find
     """
-    print "Looking for unique readings in {} in db {}:{}".format(', '.join(witnesses), db, table)
+    if not quiet:
+        print "\n\nLooking for unique readings in {} in db {}:{}".format(', '.join(witnesses), db, table)
+    else:
+        sys.stdout.write('+')
+        sys.stdout.flush()
 
     db = MySQLdb.connect(host=host, user=user, passwd=password, db=db, charset='utf8')
     cur = db.cursor()
@@ -37,7 +42,8 @@ def compare(host, db, user, password, table, witnesses):
     wit1_readings = list(cur.fetchall())
     our_witnesses = set(witnesses)
 
-    print
+    if not quiet:
+        print
     for i, (vu_id, ident, greek) in enumerate(wit1_readings):
         # Find all witnesses with that reading
         query = """SELECT witness FROM {}_ed_map
@@ -45,39 +51,13 @@ def compare(host, db, user, password, table, witnesses):
                    AND ident=%s""".format(table)
         cur.execute(query, (vu_id, ident))
         support = set(x[0] for x in cur.fetchall())
-        sys.stdout.write("\r{} of {}:\t{}   ".format(i + 1, len(wit1_readings), len(support)))
-        sys.stdout.flush()
+        if not quiet:
+            sys.stdout.write("\r{} of {}:\t{}   ".format(i + 1, len(wit1_readings), len(support)))
+            sys.stdout.flush()
 
         if support == our_witnesses:
-            print "\nMATCH", vu_id, vu_mapping[vu_id], greek
-            print
+            print "\nUnique reading found ({})".format([', '.join(witnesses)]), vu_id, vu_mapping[vu_id], greek, "\n"
 
-            #~
-        #~
-    #~ n = 0
-    #~ w1_missing = 0
-    #~ w2_missing = 0
-    #~ for row in cur.fetchall():
-        #~ if not row:
-            #~ break
-#~
-        #~ if row[1] is None:
-            #~ w1_missing += 1
-            #~ continue
-        #~ if row[2] is None:
-            #~ w2_missing += 1
-            #~ continue
-#~
-        #~ print u"VU {}: \n\t{}:\t{}\n\t{}:\t{}".format(row[0], witnesses[0], row[1], witnesses[1], row[2])
-        #~ n += 1
-#~
-    #~ print "***\n> Showing {} differences between {} and {}".format(n, witnesses[0], witnesses[1])
-    #~ if w1_missing:
-        #~ print " > {} is missing in {} places of difference".format(witnesses[0], w1_missing)
-    #~ if w2_missing:
-        #~ print " > {} is missing in {} places of difference".format(witnesses[1], w2_missing)
-#~
-    #~ return n
 
 if __name__ == "__main__":
     import argparse
@@ -88,12 +68,30 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--mysql-host', required=True, help='Host to connect to')
     parser.add_argument('-d', '--mysql-db', required=True, help='Database to connect to')
     parser.add_argument('-t', '--mysql-table', required=True, help='Table name to get data from')
+    parser.add_argument('--subsets', default=False, action='store_true', help='Try to find unique readings in all possible subsets - THIS COULD TAKE A LONG TIME')
+    parser.add_argument('-q', '--quiet', default=False, action='store_true', help='Suppress lots of output')
 
     args = parser.parse_args()
 
-    compare(args.mysql_host,
-            args.mysql_db,
-            args.mysql_user,
-            args.mysql_password,
-            args.mysql_table,
-            [x.replace(',', '') for x in args.witness])
+    witnesses = [x.replace(',', '') for x in args.witness]
+
+    if args.subsets:
+        for size in range(2, len(args.witness)):
+            for subset in itertools.combinations(witnesses, size):
+                compare(args.mysql_host,
+                        args.mysql_db,
+                        args.mysql_user,
+                        args.mysql_password,
+                        args.mysql_table,
+                        list(subset),
+                        args.quiet)
+    else:
+        compare(args.mysql_host,
+                args.mysql_db,
+                args.mysql_user,
+                args.mysql_password,
+                args.mysql_table,
+                witnesses,
+                args.quiet)
+
+    print "\n\nDone"
