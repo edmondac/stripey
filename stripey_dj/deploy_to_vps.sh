d819af886f8a9da5670562a5cad42f4c67409182
roo <devnull@localhost>
2015-06-30 09:54:58 +0100

    Deploying to VPS
    Former-commit-id: d561cdf21dcde808e8b2b80f6c05b26930b75591

--- stripey_dj/deploy_to_vps.sh
+++ stripey_dj/deploy_to_vps.sh
@@ -9,10 +9,39 @@ echo "Commit locally and deploy to VPS? [Y/n]"
 read ok
 [[ ${ok} == 'n' ]] && fatal "Aborting"
 
-set -x
-
 hg commit -m "Deploying to VPS" || echo "Continuing..."
 
 msg="$(hg summary | head -1)"
 
 scp -r stripey_app django@***REMOVED***:***REMOVED***/.
+
+echo "Copy postgres database to VPS? [y/N]"
+read ok
+[[ ${ok} != 'n' ]] && fatal "Aborting"
+
+dumpfile="django_$(date +%s).sql"
+
+# Dump all stripey_app* tables:
+echo "Please enter postgres user django's password:"
+pg_dump -U django -W -p 5434 -d django -t stripey_app* > /tmp/${dumpfile}
+
+scp /tmp/${dumpfile} django@***REMOVED***:
+
+cat > import.sh << EOF
+# Import script for ${dumpfile}
+set -x
+set -e
+echo "Finding stripey_app* tables"
+psql django -c "SELECT table_name FROM information_schema.tables WHERE table_name ILIKE 'stripey_app%';" | grep stripey > stripey_app.tables
+
+echo "Dropping stripey_app* tables"
+psql django -c "DROP TABLE \$(perl -pe 's/\n/\$1,/' stripey_app.tables))"
+
+echo "Importing new data"
+psql django < ${dumpfile}
+EOF
+
+scp import.sh django@***REMOVED***:
+rm import.sh
+ssh django@***REMOVED*** "bash import.sh"
+
